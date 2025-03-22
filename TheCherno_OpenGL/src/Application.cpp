44aaf754a8cdf6adaa5b5a9fd8bc68e6fd00bb82
MartinaPlumari <73446448+MartinaPlumari@users.noreply.github.com>
2025-03-22 01:12:00 +0100

    [EDIT] triangle rendering using buffers

--- TheCherno_OpenGL/src/Application.cpp
+++ TheCherno_OpenGL/src/Application.cpp
@@ -28,18 +28,37 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    /* Modern OpenGL -> creiamo un buffer che contiene i dati dei vertici */
+    //Il buffer viene creato una volta sola, non serve crearlo ad ogni frame, faremo invece la draw ad ogni frame
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f, 0.5f,
+         0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); //qui diciamo alla state machine come interpretare il buffer che abbiamo creato
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+
     /* Loop until the user closes the window */
+    //questo é come un game loop -> viene eseguito ad ogni frame
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0, 255, 255);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        /*Legacy OpenGL*/
+        /* glBegin(GL_TRIANGLES);
+           glColor3f(0, 255, 255);
+           glVertex2f(-0.5f, -0.5f);
+           glVertex2f(0.0f, 0.5f);
+           glVertex2f(0.5f, -0.5f);
+           glEnd();                */
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);

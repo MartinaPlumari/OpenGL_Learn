1b06062f3a9c05d82bfc71452612289d495d11fb
MartinaPlumari <73446448+MartinaPlumari@users.noreply.github.com>
2025-03-23 18:46:42 +0100

    [ADD] inline vertex and fragmente shader

--- TheCherno_OpenGL/src/Application.cpp
+++ TheCherno_OpenGL/src/Application.cpp
@@ -3,6 +3,58 @@
 
 #include <iostream>
 
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str(); //torna il puntatore al primo elemento di src
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    //Error handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        //funzione che alloca dinamicamente nello stack (invece che nell'heap)
+        char* message = (char *) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << 
+            (type == GL_VERTEX_SHADER ? "vertex " : "fragment ") 
+            << "shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+}
+
+/* Questa funzione linka i due shader in un'unica entità in modo da passarla ad OpenGL per la compilazione*/
+//per semplicità scriviamo gli shader in delle stringhe (source code). Per farlo in modo più pulito e professionale conviene metterli in un file
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    //attach our shaders to our program
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    //una volta creato il programma possiamo eliminare gli shader
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -10,7 +62,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-        
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -35,12 +87,40 @@ int main(void)
          0.0f, 0.5f,
          0.5f, -0.5f
     };
-    
+
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer); //qui diciamo alla state machine come interpretare il buffer che abbiamo creato
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    std::string vertexShader =
+        R"glsl(#version 330 core
+        
+        layout(location = 0) in vec4 position;
+
+        void main()
+        {
+            gl_Position = position;
+        }  
+        )glsl";
+
+    /*Il vertex shader in questo caso setta semplicemente il colore dei pixel a rosso*/
+    std::string fragmentShader =
+        R"glsl(#version 330 core
+        
+        layout(location = 0) out vec4 color;
+
+        void main()
+        {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }  
+        )glsl";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     //questo é come un game loop -> viene eseguito ad ogni frame
@@ -67,6 +147,8 @@ int main(void)
         glfwPollEvents();
     }
 
+    glDeleteShader(shader);
     glfwTerminate();
+
     return 0;
 }
